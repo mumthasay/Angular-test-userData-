e8b376c0b16c7cf0981660cfb1231dc14d73f663
mumthasay <mumthasay99@gmail.com>
2023-03-23 06:50:50 +0530

    userdata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserData } from './model/user-data';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'currencyConverterApp';
+userData:UserData[]=[]
+
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit() {
+    this.api.getData().subscribe((data:any) => {
+      console.log(data);
+this.userData = data
+    });
+  }
 }
---
+++ src/app/model/user-data.ts
@@ -0,0 +1,6 @@
+export interface UserData {
+id?:number,
+userId?:string,
+title?:string,
+body?:string
+}
